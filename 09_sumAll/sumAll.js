707a51561399801908c13f45820f200ddc4fc6a7
Connor Neill <cwmn1975@comcast.net>
2025-06-23 15:36:37 -0400

    09 solution
    Edited sumAll.js to include function solution.
    Edited ...spec.js to run all tests.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
 
+    if (!typeof(a) == 'number' || !typeof(b) == 'number') {
+        return "ERROR";
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else if ((a < 0) || (b < 0)) {
+        return "ERROR"
+    } else {
+        if (a > b) {
+            [a, b] = [b, a];
+        }
+        for (let i = a; i <= b; i++) {
+                total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
