d3c95e9d4b2d2a612ca8875fefdd2124bc7ace64
Connor Neill <cwmn1975@comcast.net>
2025-06-23 15:19:11 -0400

    08 solution
    Edited removeFromArray.js to include function solution.
    Also edited ...spec.js to run all tests.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+// Iteration method
+// const removeFromArray = function(arr, ...args) {
+//     const newArray = [];
+
+//     arr.forEach((item) => {
+//         if (!args.includes(item)) {
+//             newArray.push(item);
+//         }
+//     });
+
+//     return newArray;
+// };
+
+// Filter method:
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((val) => !args.includes(val));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
