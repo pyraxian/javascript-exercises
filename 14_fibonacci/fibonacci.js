0e6009ab884bec634da4adebc4f12a957f41fef0
Connor Neill <cwmn1975@comcast.net>
2025-08-05 14:39:08 -0400

    Soultion to 14
    Filled out fibonacci.js function with exercise solution.
    Modified .spec.js to run all tests.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibCount = parseInt(num);
+        
+    if (fibCount < 0 ) return "OOPS";
 
+    let previous1 = 0;
+    let previous2 = 0;
+    let next = 0;
+
+    for (let i = 0; i < Number(num); i++) {
+        next = previous1 + previous2;
+        if (next == 0) next++;
+        previous2 = previous1;
+        previous1 = next;
+        // console.log(`Current number is ${next}`);
+    }
+
+    return next;
 };
 
 // Do not edit below this line
